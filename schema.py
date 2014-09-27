42a018efa1d957721901ab195d09775d9a78111f
Kirill Brylev <brylevkirill@gmail.com>
2014-09-27 16:14:41 +0400

    Make me beautiful!

--- schema.py
+++ schema.py
@@ -51,57 +51,55 @@ def import_class(aps_schema, graph, validate_types=False):
 
         cls.subClassOf = [impl_cls]
 
-    if aps_schema.has_key('properties'):
-        for prop_key, prop in aps_schema['properties'].items():
-            Property(ns[prop_key],
-                baseType=OWL_NS.DatatypeProperty,
-                graph=graph)
+    for prop_key, prop in aps_schema.get('properties', {}).items():
+        Property(ns[prop_key],
+            baseType=OWL_NS.DatatypeProperty,
+            graph=graph)
 
-    if aps_schema.has_key('relations'):
-        for rel_key, rel in aps_schema['relations'].items():
-            type = rel['type']
-            rel_cls_list = []
-            inverse_of = None
+    for rel_key, rel in aps_schema.get('relations', {}).items():
+        type = rel['type']
+        rel_cls_list = []
+        inverse_of = None
 
-            for type in isinstance(type, list) and type or [type]:
-                rel_cls = get_class(type, graph, exists=validate_types or None)
-                if validate_types and not isinstance(rel_cls, Class):
-                    raise Exception("type %s not imported" % type)
-                rel_cls_list.append(rel_cls)
+        for type in isinstance(type, list) and type or [type]:
+            rel_cls = get_class(type, graph, exists=validate_types or None)
+            if validate_types and not isinstance(rel_cls, Class):
+                raise Exception("type %s not imported" % type)
+            rel_cls_list.append(rel_cls)
 
-                if isinstance(rel_cls, Class):
-                    parents = [cls.identifier]
-                    parents += [p.identifier for p in cls.parents]
+            if isinstance(rel_cls, Class):
+                parents = [cls.identifier]
+                parents += [p.identifier for p in cls.parents]
 
-                    for prop, _, _ in graph.triples_choices(
-                        (None, RDFS.range, parents)):
-                        if prop == ns[rel_key]:
-                            continue
+                for prop, _, _ in graph.triples_choices(
+                    (None, RDFS.range, parents)):
+                    if prop == ns[rel_key]:
+                        continue
 
-                        rev_cls = next(graph.objects(prop, RDFS.domain), None)
-                        if rev_cls is None:
-                            continue
+                    rev_cls = next(graph.objects(prop, RDFS.domain), None)
+                    if rev_cls is None:
+                        continue
 
-                        rev_cls = get_class(rev_cls, graph)
+                    rev_cls = get_class(rev_cls, graph)
 
-                        if rev_cls and isinstance(rev_cls, Class):
-                            parents = [rev_cls.identifier]
-                            parents += [p.identifier for p in rev_cls.parents]
+                    if rev_cls and isinstance(rev_cls, Class):
+                        parents = [rev_cls.identifier]
+                        parents += [p.identifier for p in rev_cls.parents]
 
-                            if rel_cls.identifier in parents:
-                                inverse_of = prop
+                        if rel_cls.identifier in parents:
+                            inverse_of = prop
 
-            prop = Property(ns[rel_key],
-                domain=cls,
-                range=rel_cls_list,
-                inverseOf=inverse_of,
-                graph=graph)
+        prop = Property(ns[rel_key],
+            domain=cls,
+            range=rel_cls_list,
+            inverseOf=inverse_of,
+            graph=graph)
 
-            if not validate_types:
-                if rel.get('required', False):
-                    cls.subClassOf = [Restriction(prop,
-                        cardinality=Literal(1),
-                        graph=graph)]
+        if not validate_types:
+            if rel.get('required', False):
+                cls.subClassOf = [Restriction(prop,
+                    cardinality=Literal(1),
+                    graph=graph)]
 
     return cls
 
